a3f6848e362dfc1e97297cbdc85d439c9c5073bd
ldibone <ldibone@ymail.com>
2023-09-22 11:23:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+//partea 1
+
+const num = 11;
+console.log(typeof num, num);
+
+const str = "Hello, World!";
+console.log(typeof str, str);
+
+const bool = true;
+console.log(typeof bool, bool);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(typeof arr, arr);
+
+const obj = { name: "Ion", age: 15 };
+console.log(typeof obj, obj);
+
+let undefinedVar;
+console.log(typeof undefinedVar, undefinedVar);
+
+const nullValue = null;
+console.log(typeof nullValue, nullValue);
+
+const bigInt = 123456789n;
+console.log(typeof bigInt, bigInt);
+
+//partea 2
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+
+var sum = num1 + num2 + num3;
+
+console.log("The sum of the three numbers is: " + sum);
+console.log("Comparison Results:");
+console.log("num1 > num2:", num1 > num2);
+console.log("num1 < num3:", num1 < num3);
+console.log("num2 === num3:", num2 === num3);
+console.log("num1 >= num2:", num1 >= num2);
+console.log("num1 <= num3:", num1 <= num3);
+console.log("num2 !== num3:", num2 !== num3);
+
+//partea 3
+
+var word1 = "Hello";
+var word2 = "World";
+
+var concatenated = word1 + " " + word2;
+console.log(concatenated); 
+
+//partea 4
+
+const myObject = {
+    nume: 'nume',
+    prenume: 'prenume',
+    hobby: 'hobby',
+    varsta: 'varsta',
+    profesie: 'profesie'
+  };
+  
+  console.log(myObject);
+  
